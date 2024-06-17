179c3759ff273530e61b30c72166acb2aaf54136
Zeeohn <successzion21@gmail.com>
2024-06-17 08:55:30 +0100

    socket correction

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -1,8 +1,10 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
+const api = import.meta.env.VITE_SOCKET_URL;
+
 const SocketContext = createContext();
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect(api);
 
 const SocketProvider = ({ children }) => {
   return (
